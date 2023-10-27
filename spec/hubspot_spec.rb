944a8176f3273df06f30d3b6feee2dcbc886e319
gildemberg-santos <gildemberg.santos@gmail.com>
2023-10-27 19:09:32 -0300

    feat: :sparkles: initial structure of the gene

---
+++ spec/hubspot_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Hubspot do
+  it "has a version number" do
+    expect(Hubspot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
